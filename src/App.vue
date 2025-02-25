a539b737abc99593a7a325e75fbd377c71518b5f
Chandler Rathke <chandler.rathke@bsu.edu>
2025-02-25 14:25:48 -0500

    gallery

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,9 @@ import GalleryPhotos from './components/Gallery.vue'
 </script>
 
 <template>
-  <!--Grocery msg="Grocery List" /-->
+  <Grocery v-if="false" msg="Grocery List" />
   <GalleryPhotos msg="Snack Gallery"/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,24 +1,54 @@
-<script>
-export default {
-
-}
+<script setup>
 defineProps({
   msg: String,
-})
+});
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <p>This is my Snack Gallery to represent how much I eat. And to convince myself to eat healthier.</p>
 
-  <div>
-    <p>Sour Patch Kids are bomb</p>
-    <img :src="require('./assets/sourpatch.webp')"/>
+  <div class="row">
+    <div class="column">
+      <p>Sour Patch Kids are bomb</p>
+      <img src="../assets/sourpatch.webp" alt="Sour Patch Kids" />
+    </div> 
+    <div class="column">
+      <p>Doritos Dinamitas are bomb</p>
+      <img src="../assets/dinamitas.webp" alt="Doritos Dinamitas" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="column">
+      <p>Cheez-Its are bomb</p>
+      <img src="../assets/cheezit.jpg" alt="Cheez-Its" />
+    </div> 
+    <div class="column">
+      <p>Goldfish (Cheddar) are bomb</p>
+      <img src="../assets/goldfish.jpg" alt="Goldfish" />
+    </div>
   </div>
 
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+img {
+  display: block;
+  margin: 0 auto;
+  width: 300px;
+  height: auto;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 50px;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
 }
 </style>
